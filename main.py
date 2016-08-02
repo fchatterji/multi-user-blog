d3fbf4ac21807797806fff2ede4844cc60755f90
fishesandchip <florent.chatterji@gmail.com>
2016-08-02 15:39:28 +0200

    Work on user authentification and permissions

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ import os
 import logging
 import time
 import hmac
+import string
+import hashlib
+import random
 
-from google.appengine.api import users
 from google.appengine.ext import ndb
 
 import jinja2
@@ -38,19 +40,50 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
+def hash_str(s):
+    return hmac.new("secret", s).hexdigest()
 
 
-"""def hash_password(password):
-    # Hash a password for the first time, with a randomly-generated salt
-    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-    return hashed_password
+def make_secure_val(s):
+    return "%s,%s" % (s, hash_str(s))
 
-def check_password(password, hashed_password):
-    # Check that a unhashed password matches one that has previously been
-    # hashed
-    return bcrypt.hashpw(password, hashed_password) == hashed_password
-"""
 
+def check_secure_val(h):
+    h = h.split(',', 1)
+    if hash_str(h[0]) == h[1]:
+        return h[0]
+    else:
+        return None
+
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+
+def make_pw_hash(name, pw, salt=make_salt()):
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+
+def check_pw_hash(name, pw, h):
+    salt = h.split(',', 1)[1]
+
+    if h == make_pw_hash(name, pw, salt=salt):
+        return True
+    else:
+        return False
+
+
+
+def login_required(handler_method):
+    """..."""
+    def check_login(self, *args):
+        if not self.user:
+            self.redirect("/blog/login")
+            return
+        else:
+            handler_method(self, *args)
+    return check_login
 
 
 
@@ -63,6 +96,10 @@ class User(ndb.Model):
     hashed_password = ndb.StringProperty(required=True)
     email = ndb.StringProperty()
 
+    @classmethod
+    def get_user_by_name(cls, name):
+        return cls.query(User.name == name).get()
+
 
 class Article(ndb.Model):
     """Article model, represents a blog post."""
@@ -71,20 +108,9 @@ class Article(ndb.Model):
     content = ndb.TextProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
     last_modified = ndb.DateTimeProperty(auto_now=True)
-    user = ndb.StructuredProperty(User)
+    author = ndb.StructuredProperty(User)
 
 
-
-
-"""
-    def set_secure_cookie(self, cookie_name, cookie_value):
-        cookie_value = hash_password(cookie_value)
-        self.response.headers.add_header(
-            'Set-Cookie',
-            '%s=%s; Path=/' % (cookie_name, cookie_value))
-
-
-"""
 
 #
 # Handlers
@@ -97,12 +123,29 @@ class BaseHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template(template)
         self.response.write(template.render(**kw))
 
+    def set_secure_cookie(self, cookie_name, cookie_value):
+        cookie_value = make_secure_val(cookie_value)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (cookie_name, cookie_value))
+
     def read_secure_cookie(self, name):
         cookie_value = self.request.cookies.get(name)
         return cookie_value
 
+    def clear_cookie(self):
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % ("user_name", ""))
+
     def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
+        user_name = self.read_secure_cookie('user_name')
+
+        self.user = user_name and User.get_user_by_name(user_name)
+
+    def check_author(self, post):
+        return self.user == post.author
 
 
 class Home(BaseHandler):
@@ -112,28 +155,33 @@ class Home(BaseHandler):
         q = Article.gql("ORDER BY created DESC")
         posts = q.fetch()
 
-        self.render('index.html', posts=posts)
+        self.render('index.html', posts=posts, user=self.user)
 
 
 class CreatePost(BaseHandler):
     """Handle new post requests."""
 
+    @login_required
     def get(self):
         self.render("create_post.html", post=None)
 
+    @login_required
     def post(self):
+
         # get the subject and content from the posted form
         post = Article(
             subject=self.request.get('subject'),
-            content=self.request.get('content'))
+            content=self.request.get('content'),
+            author=self.user)
 
         if post.subject and post.content:
 
             # save the new post in the datastore and return a key
             post_key = post.put()
 
+            post_key = post_key.urlsafe()
             # redirect to the post page
-            self.redirect('/blog/post/%s' % post_key.urlsafe())
+            self.redirect('/blog/post/%s' % post_key)
 
         else:
             # reload the page, keeping existing input, add an error message
@@ -144,6 +192,7 @@ class CreatePost(BaseHandler):
 class UpdatePost(BaseHandler):
     """Handle update post requests."""
 
+    @login_required
     def get(self, post_key):
 
         # retrieve the key from the url string
@@ -152,9 +201,12 @@ class UpdatePost(BaseHandler):
         # retrieve the post from the key
         post = post_key.get()
 
+        self.author_required(post)
+
         # render the page with the post
         self.render("create_post.html", post=post)
 
+    @login_required
     def post(self, post_key):
 
         # retrieve the key from the url string
@@ -163,12 +215,16 @@ class UpdatePost(BaseHandler):
         # retrieve the post from the key
         post = post_key.get()
 
+        self.author_required(post)
+
         # retrieve the posted subject and content
         subject = self.request.get('subject')
         content = self.request.get('content')
+        author = self.user
 
         post.subject = subject
         post.content = content
+        post.author = author
 
         if subject and content:
 
@@ -187,6 +243,7 @@ class DetailPost(BaseHandler):
     """Display the details of a post."""
 
     def get(self, post_key):
+
         # retrieve the key from the url string
         post_key = ndb.Key(urlsafe=post_key)
 
@@ -199,15 +256,20 @@ class DetailPost(BaseHandler):
 class DeletePost(BaseHandler):
     """Handle delete post requests."""
 
+    @login_required
     def get(self, post_key):
+
         # retrieve the key from th url string
         post_key = ndb.Key(urlsafe=post_key)
+        post = post_key.get()
+
+        self.author_required(post)
 
         # delete the key (and the post associated with it)
         post_key.delete()
 
-        # go to confirmation page (add a small delay of 0.5 seconds before redirecting
-        # so datastore has time to update
+        # go to confirmation page (add a small delay of 0.5 seconds 
+        # before redirecting so datastore has time to update
         time.sleep(0.5)
         self.redirect('/blog/')
 
@@ -218,33 +280,80 @@ class Register(BaseHandler):
         self.render("register.html", user=None)
 
     def post(self):
-        user = User(
-            name=self.request.get('name'),
-            password=self.request.get('password'),
-            email=self.request.get('email'))
+        
+        name = self.request.get('name')
+        password = self.request.get('password')
+        email = self.request.get('email')
 
-        if not(user.name and user.password):
+        user = User.get_user_by_name(name)
+        logging.info(user)
+
+        if user:
+            
+            error = "Name already used"
+            self.render('register.html', user=None, error=error)
+            return
+
+        hashed_password = make_pw_hash(name, password)
+
+        user = User(
+            name=name,
+            hashed_password=hashed_password,
+            email=email)
+
+        if (user.name and user.hashed_password):
+
+            user.put()
+            self.set_secure_cookie("user_name", str(user.name))
+            time.sleep(0.5)
+            self.redirect('/blog/')
+
+        else:
             error = "You need to fill out your name and password"
             self.render('register.html', user=user, error=error)
 
-        if (user.name and user.password):
 
-            """user_key = user.put()"""
+class Login(BaseHandler):
 
-            # redirect to the post page
-            self.redirect('/blog/post/%s' % post_key.urlsafe())
+    def get(self):
+        self.render("login.html", user=None)
+
+    def post(self):
+        
+        name = self.request.get('name')
+        password = self.request.get('password')
+
+        if name and password:
+
+            user = User.get_user_by_name(name)
+            logging.info(user)
+
+            if user and check_pw_hash(name, password, user.hashed_password):
+                self.set_secure_cookie("user_name", str(user.name))
+                self.redirect('/blog/')
+
+            else:
+                error = "Invalid login"
+                self.render('login.html', name=name, error=error) 
 
         else:
-            # reload the page, keeping existing input, add an error message
-            pass
+            error = "You need to fill out your name and password"
+            self.render('login.html', name=name, error=error)
 
 
+class Logout(BaseHandler):
+
+    def get(self):
+        self.clear_cookie()
+        self.redirect('/blog/')
 #
 # url scheme
 #
 app = webapp2.WSGIApplication([
     ('/blog/?', Home),
     ('/blog/register', Register),
+    ('/blog/login', Login),
+    ('/blog/logout', Logout),
     ('/blog/create', CreatePost),
     ('/blog/post/(\S+)', DetailPost),
     ('/blog/update/(\S+)', UpdatePost),
